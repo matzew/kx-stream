9ed6ec9cee5f2052a694bc92503ed6d5acc59d96
Matthias Wessendorf <matzew@apache.org>
2018-02-12 21:54:08 +0100

    :boom: first version of some rx plays

---
+++ src/main/java/net/wessendorf/rx/kafka/Runner.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright 2018, Matthias Wessendorf
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.wessendorf.rx.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Runner {
+
+    private static final DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss:SSS z dd MMM yyyy");
+
+    public static void main(String[] args) {
+
+        final KafkaStream<ConsumerRecord<String, String>> src = new KafkaStream("demo-topic");
+        src.subscribe(
+                record -> {
+                    System.out.println("We got this:  " + record.value() + ", time: " + dateFormat.format(new Date(record.timestamp())));
+                },
+                Throwable::printStackTrace,
+                () -> {}
+        );
+    }
+}
